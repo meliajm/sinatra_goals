8bccfcf9016d21fcc82331596057c50371f0b587
github username <github email address>
2020-02-09 12:49:34 -0800

    added user routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
+    enable :sessions #unless test?
     set :session_secret, "goals_application_secret" # do something here to encode?
   end
 
   helpers do 
     def logged_in?
-        !!sessions[:user_id]
+        !!session[:user_id]
     end
-    
+
     def current_user
-        User.find(sessions[:user_id])
+        User.find(session[:user_id])
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,47 +6,58 @@ class UsersController < ApplicationController
     # routes for user views
 
     get '/' do 
-        erb :'users/index'
+        if logged_in?
+            redirect to '/goals'
+        else
+            erb :'users/index'
+        end
     end
 
     get '/signup' do 
-        erb :'users/signup'
+        if logged_in?
+            redirect to '/goals'
+        else
+            erb :'users/new'
+        end
     end
 
     post "/signup" do 
         if params[:username] == "" || params[:email] == "" || params[:password] == ""
-            redirect to '/'
+            redirect to '/signup'
         else
-            User.create(username: params[:username], email: params[:email], password: params[:password])
-            redirect to '/goals/goals'
+            @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            session[:user_id] = @user.id 
+            redirect to '/goals'
         end
     end
 
     # get '/goals/goals'
 
     get '/login' do 
-        erb :'users/login'
+        if logged_in?
+            redirect to '/goals'
+        else
+            erb :'users/login'
+        end
     end
 
     post '/login' do
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(param[:password])
             session[:user_id] = @user.id 
-            redirect to 'goals/goals'
+            redirect to '/goals'
+        else
+            redirect to '/signup' if !logged_in?
+        end
+    end
+
+    get "/logout" do 
+        if logged_in?
+            session.clear
+            redirect to "/login"
         else
             redirect to '/'
         end
     end
-
-    get "logout" do 
-        session.clear
-        redirect to "/"
-    end
-
-    get '/owners/:id/edit' do
-        
-    end
-
-
 
 end
